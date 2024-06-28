6ffb057f265e6215a12fbdfaefb5d0791ea7e20f
Sony2k20 <cong.son.nguyen90@gmail.com>
2024-06-28 11:57:35 +0200

    WIP Dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,99 +1,103 @@
-import {Component, OnDestroy} from '@angular/core';
-import { NbThemeService } from '@nebular/theme';
-import { takeWhile } from 'rxjs/operators' ;
-import { SolarData } from '../../@core/data/solar';
-
-interface CardSettings {
-  title: string;
-  iconClass: string;
-  type: string;
-}
+import { Component, Input, ViewChild } from "@angular/core";
+import { NbColorHelper, NbThemeService } from "@nebular/theme";
+import * as Papa from "papaparse";
 
 @Component({
-  selector: 'ngx-dashboard',
-  styleUrls: ['./dashboard.component.scss'],
-  templateUrl: './dashboard.component.html',
+  selector: "ngx-dashboard",
+  styleUrls: ["./dashboard.component.scss"],
+  templateUrl: "./dashboard.component.html",
 })
-export class DashboardComponent implements OnDestroy {
+export class DashboardComponent {
+  @Input()
+  source = {};
 
-  private alive = true;
+  options: any;
+  data: {};
+  themeSubscription: any;
 
-  solarValue: number;
-  lightCard: CardSettings = {
-    title: 'Light',
-    iconClass: 'nb-lightbulb',
-    type: 'primary',
-  };
-  rollerShadesCard: CardSettings = {
-    title: 'Roller Shades',
-    iconClass: 'nb-roller-shades',
-    type: 'success',
-  };
-  wirelessAudioCard: CardSettings = {
-    title: 'Wireless Audio',
-    iconClass: 'nb-audio',
-    type: 'info',
-  };
-  coffeeMakerCard: CardSettings = {
-    title: 'Coffee Maker',
-    iconClass: 'nb-coffee-maker',
-    type: 'warning',
-  };
+  constructor() {
+    // private theme: NbThemeService
+    this.data = {
+      labels: [
+        "Eating",
+        "Drinking",
+        "Sleeping",
+        "Designing",
+        "Coding",
+        "Cycling",
+        "Running",
+      ],
+      datasets: [
+        {
+          data: [65, 59, 90, 81, 56, 55, 40],
+          label: "Series A",
+          borderColor: 2,
+          backgroundColor: NbColorHelper.hexToRgbA("#516e9c", 0.5),
+        },
+        {
+          data: [28, 48, 40, 19, 96, 27, 100],
+          label: "Series B",
+          borderColor: 2,
+          backgroundColor: NbColorHelper.hexToRgbA("#516e9c", 0.5),
+        },
+      ],
+    };
 
-  statusCards: string;
-
-  commonStatusCardsSet: CardSettings[] = [
-    this.lightCard,
-    this.rollerShadesCard,
-    this.wirelessAudioCard,
-    this.coffeeMakerCard,
-  ];
-
-  statusCardsByThemes: {
-    default: CardSettings[];
-    cosmic: CardSettings[];
-    corporate: CardSettings[];
-    dark: CardSettings[];
-  } = {
-    default: this.commonStatusCardsSet,
-    cosmic: this.commonStatusCardsSet,
-    corporate: [
-      {
-        ...this.lightCard,
-        type: 'warning',
+    this.options = {
+      responsive: true,
+      maintainAspectRatio: false,
+      scaleFontColor: "white",
+      legend: {
+        labels: {
+          fontColor: 1,
+        },
       },
-      {
-        ...this.rollerShadesCard,
-        type: 'primary',
+      scale: {
+        // pointLabels: {
+        //   fontSize: 14,
+        //   fontColor: 3,
+        // },
+        gridLines: {
+          color: 4,
+        },
+        angleLines: {
+          color: 5,
+        },
       },
-      {
-        ...this.wirelessAudioCard,
-        type: 'danger',
-      },
-      {
-        ...this.coffeeMakerCard,
-        type: 'info',
-      },
-    ],
-    dark: this.commonStatusCardsSet,
-  };
-
-  constructor(private themeService: NbThemeService,
-              private solarService: SolarData) {
-    this.themeService.getJsTheme()
-      .pipe(takeWhile(() => this.alive))
-      .subscribe(theme => {
-        this.statusCards = this.statusCardsByThemes[theme.name];
-    });
-
-    this.solarService.getSolarData()
-      .pipe(takeWhile(() => this.alive))
-      .subscribe((data) => {
-        this.solarValue = data;
-      });
+    };
   }
 
-  ngOnDestroy() {
-    this.alive = false;
+  dkbFields = [
+    "Buchungsdatum",
+    "Wertstellung",
+    "Status",
+    "Zahlungspflichtige*r",
+    "Zahlungsempfänger*in",
+    "Verwendungszweck",
+    "Umsatztyp",
+    "IBAN",
+    "Betrag (€)",
+    "Gläubiger-ID",
+    "Mandatsreferenz",
+    "Kundenreferenz",
+  ];
+
+  @ViewChild("fileImportInput") fileImportInput: any;
+
+  fileChangeListener($event: any): void {
+    const file: File = $event.target.files[0];
+    new Promise((resolve, reject) => {
+      Papa.parse(file, {
+        header: true,
+        complete: (result) => {
+          console.log(result);
+          console.log(result["meta"]["fields"]);
+          resolve(result.data);
+        },
+        error: (error) => {
+          reject(error);
+        },
+      });
+    });
   }
 }
